f27d21f4fcc53bc4e2e96fbfa5b8b03b0f1344d0
Jacob Vine <jvine1235@gmail.com>
2022-02-23 13:27:13 -0500

    Completed Assignment, first draft, return after for completing extra credit deliverables; drop down, priority and sort

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let taskDescription = document.querySelector("form");
+  taskDescription.addEventListener("submit", (e) => {
+    e.preventDefault();
+    createTodo(e.target.new_task_description.value);
+    taskDescription.reset();
+  });
 });
+
+function createTodo(todo){
+  let btn = document.createElement('button');
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = " x ";
+  let p = document.createElement('p');
+  p.textContent = `${todo}  `;
+  document.querySelector("#tasks").appendChild(p);
+  p.appendChild(btn);
+};
+
+function handleDelete(e){
+  e.target.parentNode.remove();
+};
